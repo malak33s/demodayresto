9729694b7f3c16f50468a60e7980f519b4b52052
malak33s <melaksarhani1bga@gmail.com>
2025-02-06 14:11:33 +0100

    lancement de app.py reussi test de toute les routes que postman reussie aussi

--- app.py
+++ app.py
@@ -4,32 +4,32 @@ app = Flask(__name__)
 
 @app.route('/menu', methods=['GET'])
 def get_menu():
-    return "hello"
+    return "Voir Menu"
 
 @app.route('/reserverTable', methods=['POST'])
 def reserver_table():
-    return "Bonjour"
+    return "Bonjour je voudrais réserver une table"
 
 @app.route('/placeOrder', methods=['POST'])
 def commander():
-    return "Hola"
+    return "Je voudrais ce plat svp"
 
 
 @app.route('/cancelReservation', methods=['DELETE'])
 def annuler_table():
-    return "Salut"
+    return "Bonjour je voudrais annuler cette réservation"
 
 @app.route('/modifyReservation', methods=['PUT'])
 def modifier_table():
-    return "Coucou"
+    return "Bonjour je voudrais modifier cette réservation"
 
 @app.route('/cancelOrder', methods=['DELETE'])
 def annuler_commande():
-    return "CIao"
+    return "annulation commande"
 
 @app.route('/modifyOrder', methods=['PUT'])
 def modifier_commande():
-    return "Aurevoir"
+    return "modification commande"
 
 if __name__ == '__main__':
     app.run(debug=True)
